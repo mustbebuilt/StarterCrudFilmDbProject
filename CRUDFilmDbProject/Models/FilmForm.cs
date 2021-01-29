359203e3a131056f58a60dd0212280c5eae85304
mustbebuilt <martin@mustbebuilt.co.uk>
2021-01-29 15:43:00 +0000

    Styled errors.

--- CRUDFilmDbProject/Models/FilmForm.cs
+++ CRUDFilmDbProject/Models/FilmForm.cs
@@ -10,21 +10,17 @@ namespace CRUDFilmDbProject.Models
         [Key]
         public int FilmID { get; set; }
 
-        [Required(ErrorMessage = "Don't make us guess.  What film are we talking about?")]
         public string FilmTitle { get; set; }
 
-        [Required]
         public string FilmCertificate { get; set; }
 
         public string FilmDescription { get; set; }
 
         public string FilmImage {  get; set;}
-
         public decimal FilmPrice { get; set; }
 
         public int Stars { get; set; }
 
-        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
     }
