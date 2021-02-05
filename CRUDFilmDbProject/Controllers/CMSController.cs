bd6acfa5f1b8b6a1502bedc664560f3f82f9de54
mustbebuilt <martin@mustbebuilt.co.uk>
2021-02-05 08:30:52 +0000

    Basic CMS index

---
+++ CRUDFilmDbProject/Controllers/CMSController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CRUDFilmDbProject.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CRUDFilmDbProject.Controllers
+{
+    public class CMSController : Controller
+    {
+        private readonly ILogger<CMSController> _logger;
+
+        private readonly ApplicationDbContext _context;
+
+        public CMSController(ILogger<CMSController> logger, ApplicationDbContext context)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Film> model = _context.Films.ToList();
+            return View(model);
+        }
+    }
+}
